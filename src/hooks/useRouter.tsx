9f4ae6cf9b47fae1cb2b97726f3c6bcf7f471585
YeongMin Ahn <ahn0min95@gmail.com>
2022-10-07 22:24:33 +0900

    chore: 주석 제거

--- src/hooks/useRouter.tsx
+++ src/hooks/useRouter.tsx
@@ -1,20 +1,4 @@
-import { Dispatch, SetStateAction, useContext } from "react";
-// import routerContext from "../context/routerContext";
-
-// import { createContext, Dispatch, SetStateAction } from "react";
-
-// interface RouterContextType {
-//   path: string;
-//   setPath: Dispatch<SetStateAction<string>>;
-// }
-
-// // context의 초기값을 통한 생성
-// const routerContext = createContext<RouterContextType>({
-//   path: "/",
-//   setPath: () => undefined,
-// });
-
-// export default routerContext;
+import { Dispatch, SetStateAction } from "react";
 
 interface useRouterProps {
   to: string;
